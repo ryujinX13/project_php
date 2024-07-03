8431a98c02fbc83d08680532bdc97d25d8c95fd0
ryujin <rewza568@gmail.com>
2024-07-03 15:39:08 +0700

    first commit

--- process/deleteProv_process.php
+++ process/deleteProv_process.php
@@ -17,7 +17,7 @@ $Prov_id = $_POST['Prov_id'];
 $sql = "DELETE FROM provider WHERE Prov_id='$Prov_id'";
 
 if ($conn->query($sql) === TRUE) {
-    echo "<script>alert('ลบข้อมูลเรียบร้อยแล้ว!!');</script>";
+    echo "<script>alert('ลบข้อมูลเรียบร้อยแล้ว!');</script>";
     echo "<script>window.location.href='../view/admin/prov_display.php'</script>";
 } else {
     echo "เกิดข้อผิดพลาด: " . $conn->error;
