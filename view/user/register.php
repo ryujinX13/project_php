3d45fd8a203079e2dfde0a3d3b20fcd4f7124d01
ryujin <rewza568@gmail.com>
2024-07-10 21:47:46 +0700

    ปรับปรุงpathแล้วก็ตกแต่งCss

--- view/user/register.php
+++ view/user/register.php
@@ -13,10 +13,10 @@
     <div class="tab-bar">
         <img src="../../img/logo1.png" alt="Logo">
         <a href="../../index.php" class="tab-link">หน้าแรก</a>
-        <a href="select_provider.php" class="tab-link">การจอง</a>
-        <a href="booking_list.php" class="tab-link">รายการจอง</a>
-        <a href="history.php" class="tab-link">ประวัติการจอง</a>
-        <a href="../provider/announce.php" class="tab-link">สมัครงาน</a>
+        <a href="booking.php" class="tab-link" id="booking-link">การจอง</a>
+        <a href="booking_list.php" class="tab-link" id="booking-list-link">รายการจอง</a>
+        <a href="history.php" class="tab-link" id="history-link">ประวัติ</a>
+        <a href="announce.php" class="tab-link announce">สมัครงาน</a>
         <a href="login_level.php" class="tab-link login">เข้าสู่ระบบ</a>
         <a href="register.php" class="tab-link register">ลงทะเบียน</a>
     </div>
