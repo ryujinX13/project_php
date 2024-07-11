79333d84e8e79d9e8101aa34d9e25600e2af5ed1
ariicct <aris22oct2002@gmail.com>
2024-07-11 15:34:17 +0700

    อีโมมาละจ้าาาาาา

--- view/user/login_level.php
+++ view/user/login_level.php
@@ -5,11 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
-    <link rel="stylesheet" type="text/css" href="../../css/user/stylslogin_level.css">
+    <link rel="stylesheet" type="text/css" href="../../css/user/styleslogin_level.css">
 </head>
 
 <body>
     <div class="tab-bar">
+        <button class="back-button" onclick="window.location.href='Homepage.php'">⬅️</button>
         <img src="../../img/logo1.png" alt="Logo" style="margin-left: 10px;">
         <h1>สิทธิ์ในการเข้าใช้งาน</h1>
     </div>
